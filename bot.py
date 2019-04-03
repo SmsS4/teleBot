eb87b0cda717282cf664b6170603398224181784
SmsS4 <36403983+SmsS4@users.noreply.github.com>
2019-04-03 18:52:02 +0430

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import sys
 import threading
 
-targetId = "mr_banana"
+targetId = "SadeghShobeiri"
 
 def echo(bot, update):
 	print("echo")
